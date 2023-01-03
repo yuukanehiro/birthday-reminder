560153ad42dc232f40350009944db665069808b0
yuu kanehiro <kanehiro@yuulinux.tokyo>
2023-01-04 00:43:47 +0900

    初期Migrationファイルのタイポ修正 (#6)
    Co-authored-by: yuu kanehiro <info@lpb-studio.com>

--- deployments/docker/db/scripts/initialize.sql
+++ deployments/docker/db/scripts/initialize.sql
@@ -1,19 +1,19 @@
 create table users
 (
-  id bigint UNIQUE,
+  id BIGINT UNIQUE,
   password TEXT,
-  insert_date timestamp with time zone,
-  update_date timestamp with time zone
+  insert_date TIMESTAMP WITH TIME ZONE,
+  update_date TIMESTAMP WITH TIME ZONE
 );
 INSERT INTO users (id, password) VALUES (1, 'secret');
 
 create table birth_days
 (
-  id bigint UNIQUE,
-  user_id bigint,
+  id BIGINT UNIQUE,
+  user_id BIGINT,
   date DATE,
-  insert_date timestamp with time zone,
-  update_date timestamp with time zone
+  insert_date TIMESTAMP WITH TIME ZONE,
+  update_date TIMESTAMP WITH TIME ZONE
 );
 INSERT INTO birth_days (id, user_id, date) VALUES (1, 1, '2018-10-20');
 INSERT INTO birth_days (id, user_id, date) VALUES (2, 1, '2018-10-21');
