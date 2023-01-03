9c42a364291f7c41d65ce7fe1d6d4e970ffd2791
yuu kanehiro <info@lpb-studio.com>
2023-01-03 13:24:22 +0900

    DBへの接続とInsert確認

--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
   "birthday-reminder/routes"
+  "birthday-reminder/databases"
 )
+
 
 func main() {
   e := routes.Init()
+
+  databases.InitDB()
+
   e.Logger.Fatal(e.Start(":8080"))
 }
