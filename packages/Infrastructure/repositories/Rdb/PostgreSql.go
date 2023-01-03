b95db18527d251a0d18b93a8f7314c253ac20427
yuu kanehiro <info@lpb-studio.com>
2023-01-03 21:21:24 +0900

    動作確認

--- packages/Infrastructure/repositories/Rdb/PostgreSql.go
+++ packages/Infrastructure/repositories/Rdb/PostgreSql.go
@@ -16,11 +16,11 @@ func NewRdb() Domain.RdbInterface {
 
 func (p postgresql) ConnectDB() {
   dsn := fmt.Sprintf(
-	"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+	  "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
 	  os.Getenv("DB_HOST"),
 	  os.Getenv("DB_USER"),
 	  os.Getenv("DB_PASSWORD"),
-	  os.Getenv("DB_DB"),
+	  os.Getenv("DB_NAME"),
 	  os.Getenv("DB_PORT"),
   )
   _, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
