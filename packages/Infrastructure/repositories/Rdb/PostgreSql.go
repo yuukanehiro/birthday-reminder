93ecfdf5a229e5a424228ed068c01998092fd1b1
yuu kanehiro <kanehiro@yuulinux.tokyo>
2023-01-04 10:03:38 +0900

    5 初期migrationファイルのタイポ修正 (#11)
    * 初期Migrationファイルのタイポ修正
    
    * buildでこけるので最新の1.19に変更。SQLに外部キー設定
    
    * Factoryと具象と抽象にフォルダ分け整理
    
    * インデント修正
    
    Co-authored-by: yuu kanehiro <info@lpb-studio.com>

--- packages/Infrastructure/repositories/Rdb/PostgreSql.go
+++ packages/Infrastructure/repositories/Rdb/PostgreSql.go
@@ -11,12 +11,12 @@ type PostgreSql struct {}
 
 func (p PostgreSql) ConnectDB() *gorm.DB {
   dsn := fmt.Sprintf(
-	  "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
-	  os.Getenv("DB_HOST"),
-	  os.Getenv("DB_USER"),
-	  os.Getenv("DB_PASSWORD"),
-	  os.Getenv("DB_NAME"),
-	  os.Getenv("DB_PORT"),
+    "host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+    os.Getenv("DB_HOST"),
+    os.Getenv("DB_USER"),
+    os.Getenv("DB_PASSWORD"),
+    os.Getenv("DB_NAME"),
+    os.Getenv("DB_PORT"),
   )
   var err error
   db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
