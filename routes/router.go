444d308b726b11eaf0b6c7a4ae3897885b767eed
yuu kanehiro <y.kanehiro@lpb-studio.com>
2023-01-06 19:23:45 +0900

    たたき台作成 (#64)
    Co-authored-by: yuu kanehiro <info@lpb-studio.com>

--- routes/router.go
+++ routes/router.go
@@ -2,7 +2,13 @@ package routes
 
 import (
   "net/http"
+  "encoding/json"
+  "time"
   "birthday-reminder/controllers"
+  "birthday-reminder/packages/Domain/Domain/Response"
+)
+const (
+  TIMESTAMP_FORMAT_RFC3339 = "2006-01-02T15:04:05Z07:00"
 )
 
 type RouterInterface interface {
@@ -13,18 +19,48 @@ type Router struct {
   i_birth_day_controller controllers.BirthDayControllerInterface
 }
 
+// construct
 func NewRouter(
   i_birth_day_controller controllers.BirthDayControllerInterface) RouterInterface {
   return &Router{i_birth_day_controller}
 }
 
-func (router Router) HandleBirthDayRequest(w http.ResponseWriter, r *http.Request) {
+// 共通APIレスポンス
+type CommonResponse struct {
+  Timestamp  string `json:"timestamp"`
+  StatusCode int `json:"status_code"`
+  Message    string `json:"message"`
+  Errors     interface{} `json:"errors"`
+  Data       interface{} `json:"data"`
+}
+
+func handle(router Router, w http.ResponseWriter, r *http.Request) interface{} {
   switch r.Method {
     case "GET":
-      router.i_birth_day_controller.ListBirthDay(w, r)
+      return router.i_birth_day_controller.ListBirthDay(w, r)
     case "POST":
-      router.i_birth_day_controller.CreateBirthDay(w, r)
+	  return router.i_birth_day_controller.CreateBirthDay(w, r)
     default:
-      w.WriteHeader(405)
+      return router.i_birth_day_controller.ListBirthDay(w, r)
   }
+}
+func timeToString(t time.Time) string {
+  str := t.Format(TIMESTAMP_FORMAT_RFC3339)
+  return str
+}
+
+func (router Router) HandleBirthDayRequest(w http.ResponseWriter, r *http.Request) {
+  result := handle(router, w, r)
+  errors := []Response.Error{}
+  response := &CommonResponse{
+    Timestamp: timeToString(time.Now()),
+	StatusCode: 200,
+	Message: "",
+	Errors: errors,
+	Data: result,
+  }
+  output, _ := json.MarshalIndent(response, "", "\t\t")
+  w.Header().Set("Content-Type", "application/json")
+  w.WriteHeader(200)
+  w.Write(output)
 }
