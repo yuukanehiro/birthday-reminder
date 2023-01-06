d7be33e5420686d51155a45880d2805ea5c9b6f7
yuu kanehiro <info@lpb-studio.com>
2023-01-06 13:17:03 +0900

    コンストラクタにまとめた

--- packages/Infrastructure/Repositories/BirthDay/BirthDayRepository.go
+++ packages/Infrastructure/Repositories/BirthDay/BirthDayRepository.go
@@ -9,39 +9,38 @@ import(
   usecase_create_birth_day "birthday-reminder/packages/UseCase/BirthDay/Create"
 )
 
-var (
-  rdb *gorm.DB
-)
-
 type BirthDayRepository struct {
+  rdb *gorm.DB
 }
 
 func NewBirthDayRepository() domain_birth_day.BirthDayRepositoryInterface {
-  connectDB()
-  return &BirthDayRepository{}
+  return &BirthDayRepository{
+    rdb: connectDB(),
+  }
 }
 
-func connectDB() {
+func connectDB() (rdb *gorm.DB) {
   cfg, err := config.NewConfig()
   if err != nil {
     log.Fatalf("failed load config. %v", err)
   }
   rdb_interface := Rdb.NewRdbFactory(cfg.DB_RDBMS)
   rdb = rdb_interface.ConnectDB()
+  return
 }
 
 func (repository_birth_day BirthDayRepository) ListBirthDay() (birth_days []domain_birth_day.BirthDay) {
-  db, _ := rdb.DB()
+  db, _ := repository_birth_day.rdb.DB()
   defer db.Close()
 
   birth_days = []domain_birth_day.BirthDay{}
-  rdb.Select("id", "user_id", "date").Find(&birth_days)
+  repository_birth_day.rdb.Select("id", "user_id", "date").Find(&birth_days)
   return
 }
 
 func (repository_birth_day BirthDayRepository) CreateBirthDay(dto usecase_create_birth_day.CreateBirthDayRequest) {
-  db, _ := rdb.DB()
+  db, _ := repository_birth_day.rdb.DB()
   defer db.Close()
 
-  rdb.Exec("INSERT INTO birth_days (user_id, date) VALUES (?, ?)", dto.UserId, dto.Date)
+  repository_birth_day.rdb.Exec("INSERT INTO birth_days (user_id, date) VALUES (?, ?)", dto.UserId, dto.Date)
 }
