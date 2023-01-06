8392d43e7c6362154bd508a4cd6046bcabaeefd3
yuu kanehiro <y.kanehiro@lpb-studio.com>
2023-01-06 13:17:23 +0900

    BirthDayRepositoryのDB呼び出し処理を少しまとめる (#53)
    * BirthDayRepositoryのDB呼び出し処理を少しまとめる
    
    * コンストラクタにまとめた
    
    Co-authored-by: yuu kanehiro <info@lpb-studio.com>

--- packages/Infrastructure/Repositories/BirthDay/BirthDayRepository.go
+++ packages/Infrastructure/Repositories/BirthDay/BirthDayRepository.go
@@ -2,45 +2,45 @@ package BirthDay
 
 import(
   "log"
+  "gorm.io/gorm"
   "birthday-reminder/config"
   "birthday-reminder/packages/Domain/Domain/Rdb"
   domain_birth_day "birthday-reminder/packages/Domain/Domain/BirthDay"
   usecase_create_birth_day "birthday-reminder/packages/UseCase/BirthDay/Create"
 )
 
-type BirthDayRepository struct {}
-
-func NewBirthDayRepository() domain_birth_day.BirthDayRepositoryInterface {
-  return &BirthDayRepository{}
+type BirthDayRepository struct {
+  rdb *gorm.DB
 }
 
+func NewBirthDayRepository() domain_birth_day.BirthDayRepositoryInterface {
+  return &BirthDayRepository{
+    rdb: connectDB(),
+  }
+}
 
-func (repository_birth_day BirthDayRepository) ListBirthDay() (birth_days []domain_birth_day.BirthDay) {
-  // todo. 共通処理化
+func connectDB() (rdb *gorm.DB) {
   cfg, err := config.NewConfig()
   if err != nil {
     log.Fatalf("failed load config. %v", err)
   }
   rdb_interface := Rdb.NewRdbFactory(cfg.DB_RDBMS)
-  rdb := rdb_interface.ConnectDB()
-  db, _ := rdb.DB()
+  rdb = rdb_interface.ConnectDB()
+  return
+}
+
+func (repository_birth_day BirthDayRepository) ListBirthDay() (birth_days []domain_birth_day.BirthDay) {
+  db, _ := repository_birth_day.rdb.DB()
   defer db.Close()
 
   birth_days = []domain_birth_day.BirthDay{}
-  rdb.Select("id", "user_id", "date").Find(&birth_days)
+  repository_birth_day.rdb.Select("id", "user_id", "date").Find(&birth_days)
   return
 }
 
 func (repository_birth_day BirthDayRepository) CreateBirthDay(dto usecase_create_birth_day.CreateBirthDayRequest) {
-  // todo. 共通処理化
-  cfg, err := config.NewConfig()
-  if err != nil {
-    log.Fatalf("failed load config. %v", err)
-  }
-  rdb_interface := Rdb.NewRdbFactory(cfg.DB_RDBMS)
-  rdb := rdb_interface.ConnectDB()
-  db, _ := rdb.DB()
+  db, _ := repository_birth_day.rdb.DB()
   defer db.Close()
 
-  rdb.Exec("INSERT INTO birth_days (user_id, date) VALUES (?, ?)", dto.UserId, dto.Date)
+  repository_birth_day.rdb.Exec("INSERT INTO birth_days (user_id, date) VALUES (?, ?)", dto.UserId, dto.Date)
 }
