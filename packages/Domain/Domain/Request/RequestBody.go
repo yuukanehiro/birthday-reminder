070dcd5c63117336fbf5d19320364d251496ca40
yuu kanehiro <info@lpb-studio.com>
2023-01-07 08:47:48 +0900

    インデント修正

--- packages/Domain/Domain/Request/RequestBody.go
+++ packages/Domain/Domain/Request/RequestBody.go
@@ -4,19 +4,19 @@ import (
   "net/http"
   "encoding/json"
   "io"
-  "fmt"
+  "log"
 )
 
 // Map request body to specified object
 func JsonDecode[T any](r *http.Request, inputData *T) {
   body, err := io.ReadAll(r.Body)
   if err != nil {
-  	fmt.Println(err)
+    log.Fatalf("failed JsonDecode when io.ReadAll. %v", err)
   }
   // finally must close
   defer r.Body.Close()
   err = json.Unmarshal(body, &inputData)
   if err != nil {
-  	fmt.Println(err)
+    log.Fatalf("failed JsonDecode when Unmarshal %v", err)
   }
 }
