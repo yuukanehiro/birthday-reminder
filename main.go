9f0e3bca9de1c616a2c8be0cde4acf0a1614d2a9
yuu kanehiro <y.kanehiro@lpb-studio.com>
2023-01-06 13:56:20 +0900

    52 modify birthdayrepository (#55)
    * BirthDayRepositoryのDB呼び出し処理を少しまとめる
    
    * コンストラクタにまとめた
    
    * 命名の変更
    
    * 無駄な記述削除
    
    * DB接続の共通処理化できた
    
    Co-authored-by: yuu kanehiro <info@lpb-studio.com>

--- main.go
+++ main.go
@@ -4,15 +4,18 @@ import (
   "fmt"
   "log"
   "net/http"
+  "gorm.io/gorm"
   "birthday-reminder/config"
   "birthday-reminder/routes"
   "birthday-reminder/controllers"
+  domain_rdb "birthday-reminder/packages/Domain/Domain/Rdb"
   app_birth_day "birthday-reminder/packages/Domain/Application/BirthDay"
   infra_repo_birth_day "birthday-reminder/packages/Infrastructure/Repositories/BirthDay"
 )
 
 
-var repo_birth_day = infra_repo_birth_day.NewBirthDayRepository()
+var rdb = connectDB()
+var repo_birth_day = infra_repo_birth_day.NewBirthDayRepository(rdb)
 var controller_birthday = controllers.NewBirthDayController(
   app_birth_day.NewListBirthDayInteractor(repo_birth_day),
   app_birth_day.NewCreateBirthDayInteractor(repo_birth_day),
@@ -29,5 +32,19 @@ func main() {
     Addr: fmt.Sprintf(":%d", cfg.WEB_PORT),
   }
   http.HandleFunc("/api/v1/birth-days/", router.HandleBirthDayRequest)
+  // DB切断
+  db, _ := rdb.DB()
+  defer db.Close()
+  // WebサーバListen
   server.ListenAndServe()
+}
+
+func connectDB() (rdb *gorm.DB) {
+  cfg, err := config.NewConfig()
+  if err != nil {
+    log.Fatalf("failed load config. %v", err)
+  }
+  rdb_interface := domain_rdb.NewRdbFactory(cfg.DB_RDBMS)
+  rdb = rdb_interface.ConnectDB()
+  return
 }
