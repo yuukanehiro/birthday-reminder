761f0a40b8200daf9bc98d9e3a6517be4e8e0a12
yuu kanehiro <y.kanehiro@lpb-studio.com>
2023-01-08 08:47:54 +0900

    RdbFactoryなるFactoryパターンをやろうとしていたが、ConfigがFactoryみたいなもので。Strategyで良かったので廃止 (#85)
    Co-authored-by: yuu kanehiro <info@lpb-studio.com>

--- packages/Infrastructure/Repositories/Rdb/Rdb.go
+++ packages/Infrastructure/Repositories/Rdb/Rdb.go
@@ -5,18 +5,20 @@ import (
   "gorm.io/gorm"
   "gorm.io/driver/postgres"
   "birthday-reminder/config"
+  domain_rdb "birthday-reminder/packages/Domain/Domain/Rdb"
 )
 
-type PostgreSql struct {}
+type Rdb struct {
+  i_rdb domain_rdb.RdbInterface
+}
 
 // construct
-func NewPostgreSql() *PostgreSql {
-  return &PostgreSql{}
+func NewRdb() domain_rdb.RdbInterface {
+  return &Rdb{}
 }
 
 // create gorm instance for PostgreSQL
-func (p PostgreSql) ConnectDB() *gorm.DB {
-  cfg := config.NewConfig()
+func (r Rdb) ConnectDB(cfg *config.Config) *gorm.DB {
   dsn := fmt.Sprintf(
     "host=%s user=%s password=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Shanghai",
     cfg.DB_HOST,
