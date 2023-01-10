690900b6cd20e93456de10a112d138728c26b3dc
yuu kanehiro <y.kanehiro@lpb-studio.com>
2023-01-11 00:01:45 +0900

    JWTからuser_id取得して誕生日登録処理を行うように改修 (#127)
    Co-authored-by: yuu kanehiro <info@lpb-studio.com>

--- packages/UseCase/BirthDay/Create/CreateBirthDayRequest.go
+++ packages/UseCase/BirthDay/Create/CreateBirthDayRequest.go
@@ -4,9 +4,15 @@ import (
   "github.com/go-playground/validator/v10"
   "time"
   "fmt"
+  "net/http"
   "birthday-reminder/packages/Domain/Domain/Response"
   "birthday-reminder/config"
+  domain_auth "birthday-reminder/packages/Domain/Domain/Auth"
+  domain_user "birthday-reminder/packages/Domain/Domain/User"
+  "birthday-reminder/packages/Domain/Domain/Request"
 )
+
+var validate *validator.Validate
 
 type CreateBirthDayRequest struct {
   UserId int64  `json:"user_id"`
@@ -24,10 +30,36 @@ type CreateBirthDayRequestValidates struct {
   Elements []CreateBirthDayRequestValidate `validate:"required,dive,required"`
 }
 
+func GetRequestBody(w http.ResponseWriter, r *http.Request) (input_data2 []CreateBirthDayRequest, errors []Response.Error) {
+  // json decode
+  input_data := []CreateBirthDayRequest{}
+  Request.JsonDecode(r, &input_data)
+  user_id := domain_auth.GetUserIdByToken(w, r)
+  input_data2 = []CreateBirthDayRequest{}
+  input_data2 = convertCreateInputData(input_data2, input_data, user_id)
+  if errors := validRequestBody(input_data2); len(errors) > 0 {
+    return nil, errors
+  }
+  return input_data2, nil
+}
+// Let the user_id bind to the object
+func convertCreateInputData(
+  result []CreateBirthDayRequest,
+  target []CreateBirthDayRequest,
+  user_id domain_user.UserId,
+) ([]CreateBirthDayRequest) {
+  for _, v := range target {
+    result = append(result, CreateBirthDayRequest{
+      UserId: user_id.GetValue(),
+      Name: v.Name,
+      Date: v.Date,
+    })
+  }
+  return result
+}
 
-var validate *validator.Validate
-
-func IsValidRequestBody(input_data []CreateBirthDayRequest) (error_array []Response.Error) {
+// isValidRequestBody() check request body
+func validRequestBody(input_data []CreateBirthDayRequest) (error_array []Response.Error) {
   var valid_format []CreateBirthDayRequestValidate
   for _, v := range input_data {
     valid_format = append(valid_format, CreateBirthDayRequestValidate{
@@ -37,10 +69,19 @@ func IsValidRequestBody(input_data []CreateBirthDayRequest) (error_array []Respo
     })
   }
   validate = validator.New()
-  validate.RegisterValidation("is_date", IsDate)
+  validate.RegisterValidation("is_date", isDate)
   return validateStruct(valid_format)
 }
+// isDate() is valid Date Format for create birth day?
+func isDate(fl validator.FieldLevel) bool {
+  _, err := time.Parse("2006-01-02", fl.Field().String())
+  if err != nil {
+    return false
+  }
+  return true
+}
 
+// validateStruct() is valid struct?
 func validateStruct(valid_format []CreateBirthDayRequestValidate) (error_array []Response.Error) {
   valid_formats := CreateBirthDayRequestValidates{Elements: valid_format}
 	err := validate.Struct(valid_formats)
@@ -60,10 +101,4 @@ func validateStruct(valid_format []CreateBirthDayRequestValidate) (error_array [
   return error_array
 }
 
-func IsDate(fl validator.FieldLevel) bool {
-  _, err := time.Parse("2006-01-02", fl.Field().String())
-  if err != nil {
-    return false
-  }
-  return true
-}
+
